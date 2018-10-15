4c3e6740635c876235604b08d7408746941afdd1
zongyanke <zongyanke.gmail.com>
2018-10-15 20:51:45 +0800

    commit

--- app.js
+++ app.js
@@ -41,4 +41,12 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
+
 module.exports = app;
